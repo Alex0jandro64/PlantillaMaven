b1f1802ba72438a0773dab37c216b202a7c39efe
Alex0jandro64 <alejandroalcerreca3@gmail.com>
2023-10-06 14:25:11 +0200

    Initial commit

---
+++ src/main/java/util/Utilidades.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Utilidades {
+	/**
+	 * Metodo que captura y devuelve una opcion entre los valores minimos y maximos
+	 * @param minimo
+	 * @param maximo
+	 * @return
+	 */
+	public int capturarOpcion(int minimo, int maximo) {
+		Scanner leerCapturaOpcion = new Scanner(System.in);
+		int opcion;
+		
+		while (true) {
+            try {
+                opcion = leerCapturaOpcion.nextInt();
+
+                if (opcion < minimo || opcion > maximo) {
+                    System.out.println("Número fuera de rango. Intente de nuevo.");
+                } else {
+                    break; // Salir del bucle si se ingresó un número válido
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada no válida. Debe ingresar un número entero.");
+                leerCapturaOpcion.next(); // Limpiar el búfer de entrada
+            }
+        }
+		leerCapturaOpcion.close();
+		return opcion;
+	}
+}
---
+++ target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/java/Utilidades.java
@@ -0,0 +1,37 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import java.${package}.InputMismatchException;
+import java.${package}.Scanner;
+
+public class Utilidades {
+	/**
+	 * Metodo que captura y devuelve una opcion entre los valores minimos y maximos
+	 * @param minimo
+	 * @param maximo
+	 * @return
+	 */
+	public int capturarOpcion(int minimo, int maximo) {
+		Scanner leerCapturaOpcion = new Scanner(System.in);
+		int opcion;
+		
+		while (true) {
+            try {
+                opcion = leerCapturaOpcion.nextInt();
+
+                if (opcion < minimo || opcion > maximo) {
+                    System.out.println("Número fuera de rango. Intente de nuevo.");
+                } else {
+                    break; // Salir del bucle si se ingresó un número válido
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada no válida. Debe ingresar un número entero.");
+                leerCapturaOpcion.next(); // Limpiar el búfer de entrada
+            }
+        }
+		leerCapturaOpcion.close();
+		return opcion;
+	}
+}
